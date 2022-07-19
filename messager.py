31a21aad8258f25134d699ae57539cb824447fde
PikaPika0710 <102190298@sv1.dut.udn.vn>
2022-07-19 20:27:27 +0700

    done messager.py

--- messager.py
+++ messager.py
@@ -11,16 +11,19 @@ website = os.getenv('WEBSITE')
 
 message = "Wish you all the best!"
 
+
 def __main__():
     chrome_service = ChromeService()
+    excel_service = ExcelService()
     chrome = chrome_service.open_browser()
     chrome_service.access_url(website + username + "/", chrome)
     chrome_service.login(username, password, chrome)
-    get_all_insta_names = ExcelService.get_column_data("data.xlsx", "Insta")
+    get_all_insta_names = excel_service.get_column_data("data.xlsx", "Insta")
     for name in get_all_insta_names:
         chrome_service.access_url(website + name + "/", chrome)
         chrome_service.send_message(message, chrome)
 
     chrome.close()
+
 
 __main__()
